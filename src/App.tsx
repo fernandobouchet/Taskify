ee99144dd9346b45745f0bcd85b9cbb71de11d00
fernandobouchet <fernandobouchet@gmail.com>
2022-03-30 22:16:15 -0300

    Add 'InputField' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import InputField from "./components/InputField";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <InputField />
     </div>
   );
 };
---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InputField = () => {
+  return (
+    <form>
+      <input type="input" placeholder="Enter a new task" />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default InputField;
